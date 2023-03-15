4aa427ca28bd5df6c5937ce4e82c0e0b010bf2bc
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-15 14:05:41 +0200

    playing the audio

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,20 @@
 <template>
   <AppHeader />
-  <RouterView></RouterView>
 
-  <!-- Player -->
-  <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
-    <!-- Track Info -->
-    <div class="text-center">
-      <span class="song-title font-bold">Song Title</span> by
-      <span class="song-artist">Artist</span>
-    </div>
-    <div class="flex flex-nowrap gap-4 items-center">
-      <!-- Play/Pause Button -->
-      <button type="button">
-        <i class="fa fa-play text-gray-500 text-xl"></i>
-      </button>
-      <!-- Current Position -->
-      <div class="player-currenttime">00:00</div>
-      <!-- Scrub Container  -->
-      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
-        <!-- Player Ball -->
-        <span class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" style="left: 50%">
-          <i class="fas fa-circle"></i>
-        </span>
-        <!-- Player Progress Bar-->
-        <span
-          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-          style="width: 50%"
-        ></span>
-      </div>
-      <!-- Duration -->
-      <div class="player-duration">03:06</div>
-    </div>
-  </div>
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mpde="out-in">
+      <component :is="Component"> </component>
+    </Transition>
+  </RouterView>
+
+  <SongPlayer />
 
   <AppAuth />
 </template>
 <script>
 import AppHeader from './components/AppHeader.vue'
 import AppAuth from './components/AppAuth.vue'
+import SongPlayer from './components/SongPlayer.vue'
 import useUserStore from '@/stores/user'
 import { mapWritableState } from 'pinia'
 import { auth } from '@/includes/firebase'
@@ -46,7 +23,8 @@ export default {
   name: 'App',
   components: {
     AppHeader,
-    AppAuth
+    AppAuth,
+    SongPlayer
   },
   computed: {
     ...mapWritableState(useUserStore, ['userLoggedIn'])
@@ -58,3 +36,17 @@ export default {
   }
 }
 </script>
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
