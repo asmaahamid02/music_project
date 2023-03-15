a25236ee4a9bb0a5932d8898e86a87e8478c3d57
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-15 14:13:25 +0200

    AppIntroduction component separated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,26 @@
 <template>
+  <AppInroduction :title="title" :description="description" />
+
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
-
+<script>
+import AppInroduction from '@/components/AppInroduction.vue'
+export default {
+  name: 'AboutView',
+  components: {
+    AppInroduction
+  },
+  data() {
+    return {
+      title: 'About Us!',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor mollis, congue augue non, venenatis elit. Nunc justo eros, suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna pulvinar, fringilla lorem eget, ullamcorper urna.'
+    }
+  }
+}
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,6 @@
 <template>
   <main>
-    <!-- Introduction -->
-    <section class="mb-8 py-20 text-white text-center relative">
-      <div
-        class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-        style="background-image: url(assets/img/header.png)"
-      ></div>
-      <div class="container mx-auto">
-        <div class="text-white main-header-content">
-          <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
-          <p class="w-full md:w-8/12 mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor mollis,
-            congue augue non, venenatis elit. Nunc justo eros, suscipit ac aliquet imperdiet,
-            venenatis et sapien. Duis sed magna pulvinar, fringilla lorem eget, ullamcorper urna.
-          </p>
-        </div>
-      </div>
-
-      <img
-        class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-        src="/assets/img/introduction-music.png"
-      />
-    </section>
+    <AppInroduction :title="title" :description="description" />
 
     <!-- Main Content -->
     <section class="container mx-auto">
@@ -42,18 +21,23 @@
 </template>
 <script>
 import { songsCollection } from '@/includes/firebase'
+import AppInroduction from '../components/AppInroduction.vue'
 import SongItem from '../components/SongItem.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    SongItem
+    SongItem,
+    AppInroduction
   },
   data() {
     return {
       songs: [],
       maxPerPage: 25,
-      pendingRequests: false
+      pendingRequests: false,
+      title: 'Listen to Great Music!',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor mollis, congue augue non, venenatis elit. Nunc justo eros, suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna pulvinar, fringilla lorem eget, ullamcorper urna.'
     }
   },
   async created() {
