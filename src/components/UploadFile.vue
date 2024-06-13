6b634d2e3c000c6375ce45ff61618b30c9a3ac9d
Asmaa Hamid <asmaahamid002@gmail.com>
2024-06-13 11:40:54 +0300

    added pwa / server worker / project configs

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -62,6 +62,19 @@ export default {
           return
         }
 
+        //check online status
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            varient: 'bg-red-400',
+            icon: 'fas fa-times',
+            text_class: 'text-red-400'
+          })
+          return
+        }
+
         const storageRef = storage.ref() //return: music-79fd8.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`) //music-79fd8.appspot.com/songs/example.mp3
 
